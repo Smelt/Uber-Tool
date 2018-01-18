7c95582dcde9478da065d8cc7c9b5a27b22d377a
Smelt <shivamsatyarthi@gmail.com>
2018-01-17 23:13:34 -0500

    set up call back for uber function

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
 const mongoose = require('mongoose');
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
 
 const schedule = require('node-schedule');
-
 const uberCaller = require('./uber.js');
-// Parsers for POST data
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-
 
 
 var rule = new schedule.RecurrenceRule();
+
+const startLatVal = '37.7752315';
+const startLongVal = '-122.418075';
+const endLongVal = '37.7752415';
+const endLatVal = '-122.518075';
+
 rule.second = 1;
 
-schedule.scheduleJob(rule, uberCaller.queryUberAPI);
+schedule.scheduleJob(rule, uberCaller.queryUberAPI(startLatVal,startLongVal,endLatVal,endLongVal));
+
+
+//schedule.scheduleJob(rule, uberCaller.queryUberAPI);
 
 
 
