5e49443dad40b207e199e2f45947ded92a407136
Smelt <shivamsatyarthi@gmail.com>
2018-01-21 04:02:42 -0500

    geo works

--- googleMaps.js
+++ googleMaps.js
@@ -11,10 +11,11 @@ exports.geoCodeAddress = function (address1, address2) {
 }
 
 var promise =  queryGeoCode('4217 Perry Hall Rd');
-promise.then(function(location){
-    console.log(location);
-});
+var px = geo('4217 Perry Hall Rd');
+console.log(px);
 
+var location = queryGeoCode3('4217 Perry Hall Rd');
+console.log(location);
 
 function queryGeoCode(address) {
     return new Promise(function(resolve, reject){
@@ -32,9 +33,63 @@ function queryGeoCode(address) {
             }
             else {
                 var results = JSON.parse(body);
-                console.log(results.results[0].geometry.location);
+               // console.log(results.results[0].geometry.location);
                 resolve(results.results[0].geometry.location);
             }
         });
     })
+}
+
+async function geo(address){
+    var promise = await queryGeoCode2(address);
+    console.log(promise);
+    
+}
+
+async function queryGeoCode2(address) {
+   return new Promise(function (resolve, reject) {
+        request({
+            qs: {
+                'address': address,
+                'key': apiKey,
+            },
+            uri: uri,
+            method: 'GET'
+        }, function (err, res, body) {
+            if (err) {
+                console.log(err);
+                reject(err);
+            }
+            else {
+                var results = JSON.parse(body);
+                // console.log(results.results[0].geometry.location);
+                resolve(results.results[0].geometry.location);
+            }
+        });
+    })
+}
+
+async function queryGeoCode3(address) {
+    var x = new Promise(function (resolve, reject) {
+        request({
+            qs: {
+                'address': address,
+                'key': apiKey,
+            },
+            uri: uri,
+            method: 'GET'
+        }, function (err, res, body) {
+            if (err) {
+                console.log(err);
+                reject(err);
+            }
+            else {
+                var results = JSON.parse(body);
+                // console.log(results.results[0].geometry.location);
+                resolve(results.results[0].geometry.location);
+            }
+        });
+    })
+    var loc = await x;
+    return loc;
 }
